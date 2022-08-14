62deaa39b3bcb8d4cc9aa21d6f717e9f0484bcda
mbarndouka <mmbarndouka@gmail.com>
2022-08-14 12:02:22 +0200

    Navbar en construction

--- src/ui/components/Navbar/Navbar.jsx
+++ src/ui/components/Navbar/Navbar.jsx
@@ -4,7 +4,15 @@ import './Navbar.css'
 export class Navbar extends Component {
   render() {
     return (
-      <div>Navbar</div>
+      <div className='Navbar'>
+        <div className="left-side">
+          leftside
+        </div>
+        <div className='vr'></div>
+        <div className="right-side">
+          rightside
+        </div>
+      </div>
     )
   }
 }
--- src/ui/pages/Home/Home.jsx
+++ src/ui/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import './Home.css'
 
+import Navbar from '../../components/Navbar/Navbar'
+
 export class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+      <div className='home'>
+        <Navbar />
+        Home
+      </div>
     )
   }
 }
