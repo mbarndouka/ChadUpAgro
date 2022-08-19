22f10cac3474b3af6d010611ddfdc9f47e0bf679
mbarndouka <mmbarndouka@gmail.com>
2022-08-19 11:03:47 +0200

    kdk

--- src/ui/pages/Home/Home.jsx
+++ src/ui/pages/Home/Home.jsx
@@ -8,7 +8,7 @@ export class Home extends Component {
     return (
       <div className='home'>
         <Navbar />
-        Home page changed
+        Home page changed ....
       </div>
     )
   }
