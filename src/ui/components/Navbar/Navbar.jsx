4867080ca618c18d58084397aa257db0693e8dd7
mbarndouka <mmbarndouka@gmail.com>
2022-09-04 22:08:03 +0200

    home navbar first part

--- src/ui/components/Navbar/Navbar.jsx
+++ src/ui/components/Navbar/Navbar.jsx
@@ -1,17 +1,13 @@
 import React, { Component } from 'react'
 import './Navbar.css'
-
+import Menu from '../../../assets/menu.svg'
+import Logo from '../../../assets/loogo.jpg'
 export class Navbar extends Component {
   render() {
     return (
       <div className='Navbar'>
-        <div className="left-side">
-          leftside
-        </div>
-        <div className='vr'></div>
-        <div className="right-side">
-          rightside
-        </div>
+        <img className='logo' src={Logo} alt="" />
+        <img src={Menu} alt="" />
       </div>
     )
   }
--- src/ui/pages/Home/Home.jsx
+++ src/ui/pages/Home/Home.jsx
@@ -1,17 +1,42 @@
-import React, { Component } from 'react'
-import './Home.css'
+import './Home.css';
+import imgr from '../../../assets/imgr.png';
+import Navbar from '../../components/Navbar/Navbar';
 
-import Navbar from '../../components/Navbar/Navbar'
-
-export class Home extends Component {
-  render() {
+export function Home() {
     return (
       <div className='home'>
-        <Navbar />
-        Home page changed ....
+        <div className="home1">
+          <div className="home-first">
+            <Navbar />
+          </div>
+          <div className="home-second">
+            <div className="title-sub">
+              <div className="title">
+                <p> Our Mission</p>
+              </div>
+              <div className="subtitle">
+                <p>Providing secure food <br/> and sustainable agriculture</p>
+              </div>
+            </div>
+            <div className="imgchad">
+              <img className='imgr' src={imgr} alt="" />
+            </div>
+            <div className="bouge-bar">
+              <div className="bouge">
+                <div className="project">
+                  <p>PROJECT</p>
+                </div>
+                <div className="content-1">
+                  <p className='head1'> Venenatis mattis pulvinar lectus</p>
+                  <p className='body1'>Malesuada egestas nam cursus massa. Cum iaculis dignissim enim eget est. Id neque, pretium ultricies tortor, tincidunt consectetur id. </p>
+                  <button className='learnM'>Learn more</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
-  }
 }
 
-export default Home
+export default Home;
